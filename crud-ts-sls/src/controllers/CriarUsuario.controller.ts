c7d618a761d8ba652409ebcf9f1ed7d89b5c50f1
michellekenia <keniamsoaress@gmail.com>
2024-01-24 18:27:18 -0300

    insere-resposta-http

--- crud-ts-sls/src/controllers/CriarUsuario.controller.ts
+++ crud-ts-sls/src/controllers/CriarUsuario.controller.ts
@@ -1,10 +1,16 @@
 import { APIGatewayProxyEvent } from "aws-lambda";
 import BaseController, { SaidaDoController } from "./BaseController";
+import { lidarComRespostaDeSucesso } from "@/utils/respostas.http";
 
 export default class CriarUsuarioController extends BaseController {
     async executar(event: APIGatewayProxyEvent): Promise<SaidaDoController> {
+       try {
         console.log("Corpo da requisição", event.body)
-        throw new Error("Method not implemented.");
+        return lidarComRespostaDeSucesso(201, {})
+       } catch (error) {
+        console.log("Erro", error)
+        return error
+       }
     }
     
 }
