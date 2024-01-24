244421695150ba0200bfebf44ef5535feae7ab4c
michellekenia <keniamsoaress@gmail.com>
2024-01-24 10:26:11 -0300

    criar-meta

--- crud-ts-sls/src/entidades/Meta.ts
+++ crud-ts-sls/src/entidades/Meta.ts
@@ -1,3 +1,28 @@
+
+export namespace MetaDados {
+    export type Entrada = {
+        titulo:string
+        objetivo: string
+        data: string
+        comentario: string 
+    }
+}
+
 export default class Meta {
+
+    titulo:string
+    objetivo: string
+    data: string
+    comentario: string
+
+    constructor(
+        parametros: MetaDados.Entrada
+    ) {
+        this.titulo = parametros.titulo
+        this.objetivo = parametros.objetivo
+        this.data = parametros.data
+        this.comentario = parametros.comentario
+
+    }
     
 }
