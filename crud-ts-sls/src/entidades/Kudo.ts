15cb4d014ce8271ccb0310ea55923066e27cd91b
michellekenia <keniamsoaress@gmail.com>
2024-01-24 10:25:45 -0300

    criar-kudo

--- crud-ts-sls/src/entidades/Kudo.ts
+++ crud-ts-sls/src/entidades/Kudo.ts
@@ -1,3 +1,32 @@
+
+export namespace KudoDados {
+    export type Entrada = {
+        agradecimento: boolean
+        alemDasExpectativas: boolean
+        liderancaInspiradora: boolean
+        data: string
+        comentario:string
+    }
+}
+
+
 export default class Kudo {
     
+    agradecimento: boolean
+    alemDasExpectativas: boolean
+    liderancaInspiradora: boolean
+    data: string
+    comentario:string
+
+    constructor(
+        parametros:KudoDados.Entrada
+    ) {
+        this.agradecimento = parametros.agradecimento
+        this.alemDasExpectativas = parametros.alemDasExpectativas
+        this.liderancaInspiradora = parametros.liderancaInspiradora
+        this.data = parametros.data
+        this.comentario = parametros.comentario
+
+    }
+
 }
