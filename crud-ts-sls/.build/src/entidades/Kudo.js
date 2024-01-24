f737cbb4d067c8956fd712d97983f9468db49011
michellekenia <keniamsoaress@gmail.com>
2024-01-24 17:21:00 -0300

    excluir pasta build

--- crud-ts-sls/.build/src/entidades/Kudo.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class Kudo {
-}
-exports.default = Kudo;
-//# sourceMappingURL=Kudo.js.map
--- crud-ts-sls/.build/src/entidades/Persona.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class Persona {
-}
-exports.default = Persona;
-//# sourceMappingURL=Persona.js.map
