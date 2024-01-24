4c86c8207b12a1d2e55e082a08c4b3eb3426f697
michellekenia <keniamsoaress@gmail.com>
2024-01-24 10:25:15 -0300

    criar-persona

--- crud-ts-sls/src/entidades/Persona.ts
+++ crud-ts-sls/src/entidades/Persona.ts
@@ -1,4 +1,22 @@
+
+export namespace PersonaDados {
+    export type Entrada = {
+        data:string
+        comentario:string
+    }
+}
+
 export default class Persona {
 
-    
+    data: string
+    comentario: string    
+
+    constructor(
+        parametros: PersonaDados.Entrada
+    ){  
+        this.data = parametros.data
+        this.comentario = parametros.comentario
+
+    }
+
 }
