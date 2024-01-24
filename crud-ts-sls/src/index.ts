fc6b9f725eff5a89ab2f3735fb4d37bbe57cf07a
michellekenia <keniamsoaress@gmail.com>
2024-01-24 17:22:28 -0300

    refatorar-rotas

--- crud-ts-sls/src/index.ts
+++ crud-ts-sls/src/index.ts
@@ -1,6 +1,8 @@
 import { APIGatewayProxyEvent } from "aws-lambda";
 import Usuario, { UsuarioDados } from "./entidades/Usuario";
 import Persona, { PersonaDados } from "./entidades/Persona";
+import dados from "./utils/bancoDeDados";
+import CriarUsuarioController from "./controllers/CriarUsuario.controller";
 
 const parametrosPersona: PersonaDados.Entrada = {
         data: "",
@@ -18,35 +20,29 @@ const parametros: UsuarioDados.Entrada = {
 }
 const usuario = new Usuario(parametros)
 
+const lidarComRespostaDeSucesso = (statusCode: number, body?:object) => ({
+  statusCode: statusCode,
+  body: JSON.stringify(body)
+})
+
 export async function buscar(event:APIGatewayProxyEvent) {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        'itens':[usuario]        
-      }
-    )
-  };
+  const usuarios = {itens: dados}
+  return lidarComRespostaDeSucesso(200, usuarios)
 }
 
 export async function criar(event:APIGatewayProxyEvent) {
-  return {
-    statusCode: 201,
-    body: JSON.stringify({})
-  };
+  const controller = new CriarUsuarioController()
+  const resposta = controller.executar(event)
+  console.log("Valor resposta" , resposta);
+  
+  return lidarComRespostaDeSucesso(201, resposta)
 }
 
 export async function atualizar(event:APIGatewayProxyEvent) {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({})
-  };
+  return lidarComRespostaDeSucesso(200, {})
 }
 
 export async function deletar(event:APIGatewayProxyEvent) {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({})
-  };
+  return lidarComRespostaDeSucesso(200, {})
 }
 
